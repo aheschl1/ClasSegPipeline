1183d84b6ea463f951317e2af96d600e761de610
aheschl1 <aheschl@gmail.com>
2024-06-03 14:04:58 -0600

    bug fixx

--- classeg/extensions/coco_seg/__init__.py
+++ classeg/extensions/coco_seg/__init__.py
@@ -1,4 +1,4 @@
 # coco_seg extension
-TRAINER_CLASS_NAME = "ClassificationTrainer"
+TRAINER_CLASS_NAME = "SegmentationTrainer"
 PREPROCESSOR_CLASS_NAME = "CocoPreprocessor"
 INFERER_CLASS_NAME = "SegmentationInferer"
