77f660a30c777a87d9464ee159ffa5bed9ea2760
aheschl1 <ajheschl@gmail.com>
2024-06-30 13:39:14 -0600

    add wloss weight

--- classeg/extensions/unstable_diffusion/training/trainer.py
+++ classeg/extensions/unstable_diffusion/training/trainer.py
@@ -50,6 +50,8 @@ class UnstableDiffusionTrainer(Trainer):
         # self._instantiate_inferer(self.dataset_name, fold, unique_folder_name)
         self.infer_every: int = 10
         self.recon_loss, self.gan_loss = self.loss
+        self.recon_weight = self.config.get("recon_weight", 0.5)
+        self.gan_weight = self.config.get("gan_weight", 0.5)
 
     @override
     def get_augmentations(self) -> Tuple[A.Compose, A.Compose]:
@@ -109,32 +111,33 @@ class UnstableDiffusionTrainer(Trainer):
             im_noise, seg_noise, images, segmentations, t = self.forward_diffuser(images, segmentations)
             # do prediction and calculate loss
             predicted_noise_im, predicted_noise_seg = self.model(images, segmentations, t)
-            loss = 0.5 * self.recon_loss(predicted_noise_im, im_noise) + 0.5 * self.recon_loss(predicted_noise_seg, seg_noise)
+            loss = self.recon_weight * self.recon_loss(predicted_noise_im, im_noise) + self.recon_weight * self.recon_loss(predicted_noise_seg, seg_noise)
             # convert x_t to x_{t-1} and descriminate the goods
-            predicted_im, predicted_seg = self.forward_diffuser.inference_call(
-                images,
-                segmentations, 
-                predicted_noise_im, 
-                predicted_noise_seg, t, clamp=False, training_time=True
-            )
-            images, segmentations = self.forward_diffuser.inference_call(
-                images,
-                segmentations, 
-                im_noise, 
-                seg_noise, t, clamp=False, training_time=True
-            )
-            t-=1
-            # make randomly 50/50 real and 50/50 fake
-            # generate a random list of n 0s and 1s, where roughly half are 1s and half are 0s
-            # where it is 1, we replace predicted_im with the original image, where it is 0, we leave it
-            real_fake_labels = torch.randint(0, 2, (images.shape[0],)).to(self.device).reshape(images.shape[0], 1, 1, 1)
-            predicted_im = real_fake_labels * images + (1 - real_fake_labels) * predicted_im
-            predicted_seg = real_fake_labels * segmentations + (1 - real_fake_labels) * predicted_seg
-            predicted_concat = torch.cat([predicted_im, predicted_seg], dim=1)
-            
-            discriminator_logits = self.model.discriminate(predicted_concat, t)
-            # calculate the loss
-            loss += 0.5 * self.gan_loss(discriminator_logits.squeeze(), real_fake_labels.squeeze().float())
+            if self.gan_weight > 0:
+                predicted_im, predicted_seg = self.forward_diffuser.inference_call(
+                    images,
+                    segmentations, 
+                    predicted_noise_im, 
+                    predicted_noise_seg, t, clamp=False, training_time=True
+                )
+                images, segmentations = self.forward_diffuser.inference_call(
+                    images,
+                    segmentations, 
+                    im_noise, 
+                    seg_noise, t, clamp=False, training_time=True
+                )
+                t-=1
+                # make randomly 50/50 real and 50/50 fake
+                # generate a random list of n 0s and 1s, where roughly half are 1s and half are 0s
+                # where it is 1, we replace predicted_im with the original image, where it is 0, we leave it
+                real_fake_labels = torch.randint(0, 2, (images.shape[0],)).to(self.device).reshape(images.shape[0], 1, 1, 1)
+                predicted_im = real_fake_labels * images + (1 - real_fake_labels) * predicted_im
+                predicted_seg = real_fake_labels * segmentations + (1 - real_fake_labels) * predicted_seg
+                predicted_concat = torch.cat([predicted_im, predicted_seg], dim=1)
+                
+                discriminator_logits = self.model.discriminate(predicted_concat, t)
+                # calculate the loss
+                loss += self.gan_weight * self.gan_loss(discriminator_logits.squeeze(), real_fake_labels.squeeze().float())
 
             # update model
             loss.backward()
@@ -168,32 +171,33 @@ class UnstableDiffusionTrainer(Trainer):
             noise_im, noise_seg, images, segmentations, t = self.forward_diffuser(images, segmentations)
 
             predicted_noise_im, predicted_noise_seg = self.model(images, segmentations, t)
-            loss = 0.5 * self.recon_loss(predicted_noise_im, noise_im) + 0.5 * self.recon_loss(predicted_noise_seg, noise_seg)
+            loss = self.recon_weight * self.recon_loss(predicted_noise_im, noise_im) + self.recon_weight * self.recon_loss(predicted_noise_seg, noise_seg)
             # convert x_t to x_{t-1} and descriminate the goods
-            predicted_im, predicted_seg = self.forward_diffuser.inference_call(
-                images,
-                segmentations, 
-                predicted_noise_im, 
-                predicted_noise_seg, t, clamp=False, training_time=True
-            )
-            images, segmentations = self.forward_diffuser.inference_call(
-                images,
-                segmentations, 
-                noise_im, 
-                noise_seg, t, clamp=False, training_time=True
-            )
-            t-=1
-            # make randomly 50/50 real and 50/50 fake
-            # generate a random list of n 0s and 1s, where roughly half are 1s and half are 0s
-            # where it is 1, we replace predicted_im with the original image, where it is 0, we leave it
-            real_fake_labels = torch.randint(0, 2, (images.shape[0],)).to(self.device).reshape(images.shape[0], 1, 1, 1)
-            predicted_im = real_fake_labels * images + (1 - real_fake_labels) * predicted_im
-            predicted_seg = real_fake_labels * segmentations + (1 - real_fake_labels) * predicted_seg
-            predicted_concat = torch.cat([predicted_im, predicted_seg], dim=1)
-            
-            discriminator_logits = self.model.discriminate(predicted_concat, t)
-            # calculate the loss
-            loss += 0.5 * self.gan_loss(discriminator_logits.squeeze(), real_fake_labels.squeeze().float())
+            if self.gan_weight > 0:
+                predicted_im, predicted_seg = self.forward_diffuser.inference_call(
+                    images,
+                    segmentations, 
+                    predicted_noise_im, 
+                    predicted_noise_seg, t, clamp=False, training_time=True
+                )
+                images, segmentations = self.forward_diffuser.inference_call(
+                    images,
+                    segmentations, 
+                    noise_im, 
+                    noise_seg, t, clamp=False, training_time=True
+                )
+                t-=1
+                # make randomly 50/50 real and 50/50 fake
+                # generate a random list of n 0s and 1s, where roughly half are 1s and half are 0s
+                # where it is 1, we replace predicted_im with the original image, where it is 0, we leave it
+                real_fake_labels = torch.randint(0, 2, (images.shape[0],)).to(self.device).reshape(images.shape[0], 1, 1, 1)
+                predicted_im = real_fake_labels * images + (1 - real_fake_labels) * predicted_im
+                predicted_seg = real_fake_labels * segmentations + (1 - real_fake_labels) * predicted_seg
+                predicted_concat = torch.cat([predicted_im, predicted_seg], dim=1)
+                
+                discriminator_logits = self.model.discriminate(predicted_concat, t)
+                # calculate the loss
+                loss += self.gan_weight * self.gan_loss(discriminator_logits.squeeze(), real_fake_labels.squeeze().float())
 
             # gather data
             running_loss += loss.item() * images.shape[0]
