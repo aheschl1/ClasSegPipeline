e02ecae6fb8818e38048fda42cb2925fc6f7686c
Mauricio <mauricio.murillo.gs@gmail.com>
2024-07-10 10:47:32 -0600

    Inference A lot

--- classeg/extensions/unstable_diffusion/inference/inferer.py
+++ classeg/extensions/unstable_diffusion/inference/inferer.py
@@ -2,14 +2,16 @@ import os
 import shutil
 from typing import Tuple
 
+import pandas as pd
+import cv2
+
 import numpy as np
 import torch
-from matplotlib import pyplot as plt
-from torchvision.utils import make_grid
 from tqdm import tqdm
 
 from classeg.dataloading.datapoint import Datapoint
 from classeg.extensions.unstable_diffusion.utils.utils import get_forward_diffuser_from_config
+from classeg.extensions.unstable_diffusion.forward_diffusers.diffusers import LinearDiffuser
 from classeg.inference.inferer import Inferer
 from classeg.utils.utils import read_json
 from classeg.utils.constants import RESULTS_ROOT
@@ -34,8 +36,6 @@ class UnstableDiffusionInferer(Inferer):
         self.forward_diffuser = get_forward_diffuser_from_config(self.config)
         self.timesteps = self.config["max_timestep"]
         self.kwargs = kwargs
-        
-        # self.model_json = read_json(f"{self.lookup_root}/model.json")
 
     def get_augmentations(self):
         ...
@@ -67,6 +67,8 @@ class UnstableDiffusionInferer(Inferer):
         Returns the output directory, and creates dataloader
         """
         save_path = f'{self.lookup_root}/inference'
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
         self.model = self._get_model().cpu()
         checkpoint = torch.load(
             f"{self.lookup_root}/{self.weights}.pth"
@@ -76,76 +78,101 @@ class UnstableDiffusionInferer(Inferer):
         return save_path
 
     def infer(self):
-        grid_size = int(self.kwargs.get("g", 1))
-        grid_size = int(self.kwargs.get("grid_size", grid_size))
+        # To infer we need the number of samples to generate, and name of folder
+        num_samples = int(self.kwargs.get("s", 1000))
+        run_name  = self.kwargs.get("r", "Inference")
 
-        save_process = self.kwargs.get("save_process", False) in [True, '1', 1, 't', 'T']
-
-        save_path = self.pre_infer()
+        # Inference generates folders with the csv file
+        save_path = f'{self.pre_infer()}/{run_name}'
         if os.path.exists(save_path):
             shutil.rmtree(save_path)
+        
         os.mkdir(save_path)
+        os.mkdir(f'{save_path}/Images')
+        os.mkdir(f'{save_path}/Masks')
+        entries = []
+
         self.model.eval()
         in_shape = list(self.config["target_size"])
         if self.config.get("super_resolution", False):
             for i in range(len(in_shape)):
                 in_shape[i] = in_shape[i] * 2
+        
+        batch_size = int(self.config.get("infer_batch_size", 32))
+        case_num = 0
         with torch.no_grad():
-            xt_im = torch.randn(
-                (
-                    grid_size ** 2,
-                    self.config["model_args"]["im_channels"],
-                    *in_shape,
-                )
-            )
-            xt_seg = torch.randn(
-               (
-                   grid_size ** 2,
-                   self.config["model_args"]["seg_channels"],
-                   *in_shape,
-               )
-            )
-            xt_im = xt_im.to(self.device)
-            xt_seg = xt_seg.to(self.device)
-            # self.timesteps = 1000
-            for t in tqdm(range(self.timesteps - 1, -1, -1), desc="running inference"):
-                time_tensor = (torch.ones(xt_im.shape[0]) * t).to(xt_im.device).long()
+            for runs in tqdm(range(0,int(np.ceil(num_samples/batch_size))), desc="running_inferences"):
+                if ((num_samples - case_num) < batch_size):
+                    batch_size = (num_samples - case_num)
+                
+                xt_im, xt_seg = self.progressive_denoise(batch_size, in_shape)
+                # Binarize the mask
+                xt_seg[xt_seg < 0.5] = 0
+                xt_seg[xt_seg > 0] = 1
 
-                noise_prediction_im, noise_prediciton_seg = self.model(
-                    xt_im, xt_seg, time_tensor
-                )
-                xt_im, xt_seg = self.forward_diffuser.inference_call(
-                    xt_im,
-                    xt_seg,
-                    noise_prediction_im,
-                    noise_prediciton_seg,
-                    t,
-                    clamp=False,
-                )
-                if save_process or t == 0:
-                    grid_im = make_grid(xt_im, nrow=grid_size)
-                    grid_im = grid_im.cpu().permute(1, 2, 0).numpy()
-                    grid_im -= grid_im.min()
-                    grid_im *= 255 / grid_im.max()
-                    grid_im = grid_im.astype(np.uint8)
-                    plt.imsave(f"{save_path}/x0_{t}_im.png", grid_im)
-
-                if save_process or t == 0:
-                    # grid_seg = make_grid(torch.from_numpy(bitmask_to_label(np.round(xt_seg.cpu().numpy()))), nrow=grid_size)
-                    grid_seg = make_grid(xt_seg.round(), nrow=grid_size)
-                    print("Waring: not unbitifying. Only good for binary")
-
-                    grid_seg = grid_seg.cpu().permute(1, 2, 0).numpy().astype(float)
-                    grid_seg -= grid_seg.min()
-                    grid_seg *= 255 / grid_seg.max()
-                    grid_seg = grid_seg.astype(np.uint8)
-                    plt.imsave(f"{save_path}/x0_{t}_seg.png", grid_seg)
-                    
-        xt_im = xt_im.cpu()[0].permute(1, 2, 0).numpy()
-        # xt_seg = bitmask_to_label(np.round(xt_seg.cpu()[0].permute(1, 2, 0).numpy()))
-        xt_seg = xt_seg.round()[0].cpu().permute(1, 2, 0).numpy()
+                xt_im = xt_im.detach().cpu()
+                xt_seg = xt_seg.detach().cpu()
+                xt_im = xt_im - xt_im.min(dim=1).values.min(dim=1).values.min(dim=1).values.reshape(-1,1,1,1)
+                xt_seg = xt_seg - xt_seg.min(dim=1).values.min(dim=1).values.min(dim=1).values.reshape(-1,1,1,1)
+                xt_im = xt_im * (255 / xt_im.max(dim=1).values.max(dim=1).values.max(dim=1).values).reshape(-1,1,1,1)
+                xt_seg = xt_seg * (255 / xt_seg.max(dim=1).values.max(dim=1).values.max(dim=1).values).reshape(-1,1,1,1)
+                
+                xt_im = xt_im.permute(0,2,3,1).numpy().astype(np.uint8)
+                xt_seg = xt_seg.permute(0,2,3,1).numpy().astype(np.uint8)
+                for i in range(batch_size):
+                    cv2.imwrite(f'{save_path}/Images/case_{case_num}.jpg', xt_im[i])
+                    cv2.imwrite(f'{save_path}/Masks/case_{case_num}.jpg', xt_seg[i])
+                    entries.append({
+                        'IID': case_num,
+                        'GID': 1,
+                        'Image': f'{save_path}/Images/case_{case_num:05}.jpg',
+                        'Mask':  f'{save_path}/Images/case_{case_num:05}.jpg',
+                        'Label': 1
+                    })
+                    case_num+=1;
+            columns = ['IID', 'GID', 'Image', 'Mask', 'Label']
+            df = pd.DataFrame(entries, columns=columns)
+            df.to_csv(f'{save_path}/generated.csv', index=False)
         return xt_im, xt_seg
 
+    def progressive_denoise(self, batch_size, in_shape, steps):
+        if steps is not None:
+            stride = int(self.timesteps // steps);
+            diffuser = LinearDiffuser(steps, self.config["max_beta"], self.config["min_beta"])
+        else:
+            stride = -1
+            diffuser = self.forward_diffuser
+        xt_im = torch.randn(
+            (
+                batch_size,
+                self.config["model_args"]["im_channels"],
+                *in_shape,
+            )
+        )
+        xt_seg = torch.randn(
+           (
+               batch_size,
+               self.config["model_args"]["seg_channels"],
+               *in_shape,
+           )
+        )
+        xt_im = xt_im.to(self.device)
+        xt_seg = xt_seg.to(self.device)
+        # self.timesteps = 1000
+        for t in tqdm(range(self.timesteps - 1, -1, -stride), desc="running inference"):
+            time_tensor = (torch.ones(xt_im.shape[0]) * t).to(xt_im.device).long()
+            noise_prediction_im, noise_prediciton_seg = self.model(
+                xt_im, xt_seg, time_tensor
+            )
+            xt_im, xt_seg = diffuser.inference_call(
+                xt_im,
+                xt_seg,
+                noise_prediction_im,
+                noise_prediciton_seg,
+                t//stride,
+                clamp=False,
+            )
+        return xt_im, xt_seg
     def post_infer(self):
         """
         Here, inference has run on every sample.
